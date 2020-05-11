492cde1d524ac8d9494b942fb18c6db38e3d31bf
MarianelaNeira <=mpneiraleiva@gmail.com>
2020-05-11 18:31:28 -0400

    updload project

---
+++ ProjectMarianelaNeira/Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class GameController : MonoBehaviour
+{
+    public static string fimeName = "JsonChallenge.json";
+    public static string tittle = string.Empty;
+    public static List<string> headers = new List<string>();
+    public static List<Dictionary<string, string>> data = new List<Dictionary<string, string>>();
+    static JObject json;
+
+    private void Awake()
+    {
+       StartCoroutine(GetDataFromFile());
+    }
+   
+    public static IEnumerator GetDataFromFile()
+    {
+        GetJsonFile();
+        GetTitle(json);
+        GetHeaders(json);
+        GetData(json);
+        yield return null;
+    }
+
+
+    #region GetData
+    private static void GetJsonFile()
+    {
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, fimeName);
+        var result = System.IO.File.ReadAllText(filePath);
+        json = JObject.Parse(result);       
+    }
+
+    private static void GetTitle(JObject json)
+    {
+        tittle = json["Title"].ToString();
+    }
+
+    private static void GetHeaders(JObject json)
+    {
+        headers.Clear();
+        foreach (var item in json["ColumnHeaders"])
+        {
+            headers.Add(item.ToString());
+        }
+    }
+
+    private static void GetData(JObject json)
+    {
+        foreach (var item in json["Data"])
+        {
+            JObject json2 = JObject.Parse(item.ToString());
+            Dictionary<string, string> value = new Dictionary<string, string>();
+
+            foreach (var i in json2)
+            {
+                value.Add(i.Key, i.Value.ToString());
+            }
+
+            data.Add(value);
+        }
+    }
+
+    #endregion
+}
---
+++ ProjectMarianelaNeira/Assets/Scripts/UIController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Text Tittle;
+    public GameObject Headers;
+    public GameObject Data;
+    Button Actualizar;
+
+    void Start()
+    {
+        UpdateData();
+    }
+    
+
+    public void ActualizarData()
+    {
+        StartCoroutine(UpdateCoroutine());       
+    }
+
+    IEnumerator UpdateCoroutine()
+    {
+        DeleteData();
+        StartCoroutine( GameController.GetDataFromFile());
+        UpdateData();
+        yield return null;
+
+    }
+    private void UpdateData()
+    {
+        Tittle.text = GameController.tittle;
+        SetTextProperties(Tittle, "Header");
+
+        foreach (var item in GameController.headers)
+        {
+            GameObject newHeaderText = new GameObject(item, typeof(RectTransform));
+            var headerText = newHeaderText.AddComponent<Text>();
+            headerText.text = item;
+            SetTextProperties(headerText, "Header");
+            newHeaderText.transform.SetParent(Headers.transform);
+        }
+
+        int count = 1;
+        foreach (var item in GameController.data)
+        {
+            GameObject newDataEmpty = new GameObject("Item " + count, typeof(RectTransform));
+            newDataEmpty.transform.SetParent(Data.transform);
+
+            RectTransform rectTransform = newDataEmpty.GetComponent<RectTransform>();
+
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Data.GetComponent<RectTransform>().rect.width);
+            rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Data.GetComponent<RectTransform>().rect.height);
+
+            newDataEmpty.AddComponent<HorizontalLayoutGroup>();
+
+           
+            foreach (var item2 in item)
+            {
+                GameObject newDataText = new GameObject(item2.Key, typeof(RectTransform));                
+                var dataText = newDataText.AddComponent<Text>();
+                dataText.text = item2.Value;
+                SetTextProperties(dataText, "Data");
+                dataText.transform.SetParent(newDataEmpty.transform);               
+            }
+            count++;
+
+        }
+    }
+
+    private void DeleteData()
+    {
+        Tittle.text = string.Empty;
+        foreach (Transform child in Headers.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        GameController.headers.Clear();
+
+        foreach (Transform child in Data.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        GameController.data.Clear();
+    }
+
+    private void SetTextProperties(Text text, string type)
+    {
+        text.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        text.color = Color.black;
+        text.alignment = TextAnchor.MiddleCenter;
+        switch (type)
+        {
+            case "Title":
+                text.fontStyle = FontStyle.Bold;
+                text.fontSize = 30;
+                break;
+            case "Header":
+                text.fontStyle = FontStyle.Bold;
+                text.fontSize = 26;
+                break;
+            case "Data":
+                text.fontSize = 22;
+                break;
+            default:
+                break;
+        }
+    }
+
+}
